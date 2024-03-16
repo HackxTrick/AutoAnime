e3405541a67dce007d9c6297603a0d477818400a
HackxTrick <153048345+HackxTrick@users.noreply.github.com>
2024-03-16 06:21:38 +0530

    Update __init__.py
    Signed-off-by: HackxTrick <153048345+HackxTrick@users.noreply.github.com>

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -13,13 +13,13 @@ LOG.setLevel(level=logging.INFO)
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',}
 
-API_ID = int(environ.get('API_ID', 45647)) #API ID
-API_HASH = environ.get('API_HASH', 'Your Api Id') #API HASH
-BOT_TOKEN = environ.get('BOT_TOKEN', 'Your Bot Token') #BOT TOKEN
-DATABASE_URL = environ.get('DATABASE_URL', 'Your MongoDb') #MONGO DB
-OWNER_ID = int(environ.get('OWNER_ID', None)) #OWNER ID
-MAIN_CHANNEL = int(environ.get('MAIN_CHANNEL', None))#YOUR MAIN CHANNEL ID
-ARCHIVE_CHANNEL = int(environ.get('ARCHIVE_CHANNEL', None))#YOUR ARCHIVE CHANNEL
-MESSAGE_ID = int(environ.get('MESSAGE_ID', None)) #SUB CHANNEL STATUS ID
+API_ID = int(environ.get('API_ID',28837889 )) #API ID
+API_HASH = environ.get('API_HASH', '9d5e9c5b8abcf8b7b930abd259de254e') #API HASH
+BOT_TOKEN = environ.get('BOT_TOKEN', '6655774727:AAGsUPOSWeVnpzwd4ME7rvBs5dNAMNViur0') #BOT TOKEN
+DATABASE_URL = environ.get('DATABASE_URL', 'mongodb+srv://Hackx:Hackx@hackx.6flteju.mongodb.net/?retryWrites=true&w=majority&appName=Hackx') #MONGO DB
+OWNER_ID = int(environ.get('OWNER_ID', '1910728581')) #OWNER ID
+MAIN_CHANNEL = int(environ.get('MAIN_CHANNEL', '-1002106866999'))#YOUR MAIN CHANNEL ID
+ARCHIVE_CHANNEL = int(environ.get('ARCHIVE_CHANNEL', '-1002007169772' ))#YOUR ARCHIVE CHANNEL
+MESSAGE_ID = int(environ.get('MESSAGE_ID', '-1001732273753')) #SUB CHANNEL STATUS ID
 
 soheru = Client('SoheruBots', api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, plugins=dict(root="Bot/plugins"))
